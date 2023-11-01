cb6a3101df45cc965f0699844bba25009649925d
Scott Vaughan <scott.vaughan@ensono.com>
2023-11-01 09:42:17 +0000

    Create PDF document in prevewer

--- CreatePDF/Program.cs
+++ CreatePDF/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+using QuestPDF.Previewer;
+
+// Configure the type of licence we are using.
+QuestPDF.Settings.License = LicenseType.Community;
+
+Document.Create(container =>
+{
+    container.Page(page =>
+    {
+        page.Size(PageSizes.A4);
+        page.Margin(2, Unit.Centimetre);
+        page.PageColor(Colors.White);
+        page.DefaultTextStyle(x => x.FontSize(20));
+        page.Header()
+            .Text("Hello World!")
+            .SemiBold()
+            .FontSize(30)
+            .FontColor(Colors.Red.Medium);
+        page.Content()
+            .PaddingVertical(1, Unit.Centimetre)
+            .Column(x =>
+            {
+                x.Spacing(20);
+                x.Item().Text(Placeholders.LoremIpsum());
+                x.Item().Image(Placeholders.Image(200, 100));
+            });
+        page.Footer()
+            .AlignCenter()
+            .Text(x =>
+            {
+                x.Span("Page ");
+                x.CurrentPageNumber();
+            });
+    });
+})
+.ShowInPreviewer();
+//.GeneratePdf("test.pdf");
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# CreatePDF
+# The Easiest Way to Create PDFs in .NET
+
+## Example of using QuestPDF to create PDF documents in .Net
+## https://youtu.be/_M0IgtGWnvE?si=YwtcFfAQ1ELI0-Uq
+
+Install the Previewer by running the global command:
+
+dotnet tool install QuestPDF.Previewer --global
